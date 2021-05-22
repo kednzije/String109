0b41a158d42326511a9bd4081ffef20acf894503
kednzije <jiuluji77773@163.com>
2021-05-22 08:30:02 +0800

    After Contest

--- NowCoder/The2021SummerVacationTrainingCampDay4/TempCalc.cpp
+++ NowCoder/The2021SummerVacationTrainingCampDay4/TempCalc.cpp
@@ -6,10 +6,16 @@ double f(double i) {
 	return i * (i + 1) * (i + 1) * (i + 2) * pow(1.0 / 3, i + 1);
 }
 
+double f(double k, double a) {
+	return pow(1.0 / 2, 2 * k - a) * (k + 1) * (k + 1) * (3.0 / 4 * (k - a) + 0.5);
+}
+
 int main() {
 	double ans = 0;
-	for(int i = 1; i <= 100000; i++) {
-		ans += f(i);
+	for(int k = 1; k <= 20000; k++) {
+		for(int a = 0; a < k; a++) {
+			ans += f(k, a);
+		}
 	}
 	cout << ans << endl;
 }
