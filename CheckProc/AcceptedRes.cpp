d6df36c86ff6415363bea3a6341ecfa461bb05e9
kednzije <jiuluji77773@163.com>
2021-08-23 10:26:24 +0800

    hihoCoder 1147

--- CheckProc/AcceptedRes.cpp
+++ CheckProc/AcceptedRes.cpp
@@ -1,211 +1,59 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define db long double
-#define x first
-#define y second
-#define mp make_pair
-#define pb push_back
-#define all(a) a.begin(), a.end()
-
 using namespace std;
+#define MOD 1000000007
+typedef long long ll;
+const int maxn = 103;
 
-const int mod = 1000000007;
+int n, m, fac[maxn], facinv[maxn], mi[maxn];
+ll dp[maxn][maxn][maxn], ans;
 
-void add(int& a, int b) {
-  a += b;
-  if (a >= mod) a -= mod;
-  if (a < 0) a += mod;
+int qmi(ll a, ll b)
+{
+	int ret = 1;
+	for (a %= MOD; b; b >>= 1, a = 1ll * a * a % MOD)
+		if (b & 1)
+			ret = 1ll * ret * a % MOD;
+	return ret;
 }
-
-int mult(int a, int b) {
-  return a * (ll)b % mod;
+int C(int n, int m)
+{
+	if (n < m)
+		return 0;
+	return 1ll * fac[n] * facinv[n - m] % MOD * facinv[m] % MOD;
 }
-
-int bp(int a, int b) {
-  int res = 1;
-  while (b > 0) {
-    if (b & 1) res = mult(res, a);
-    a = mult(a, a);
-    b >>= 1;
-  }
-  return res;
-}
-
-vector<int> w;
-vector<vector<pair<int, int> > > gr;
-
-vector<bool> used;
-vector<bool> eused;
-vector<vector<int> > tot;
-
-void dfs(int vertex, int led, vector<pair<int, int> > &path) {
-
-	path.pb(mp(vertex, led));
-	used[vertex] = true;
-
-	for (auto to : gr[vertex]) {
-		if (used[to.x]) {
-			if (eused[to.y]) continue;
-			if (to.y == led) continue;
-			vector<int> G;
-			for (int i = path.size() - 1; i >= 0; --i) {
-				G.pb(path[i].y);
-				if (path[i - 1].x == to.x) break;
-			}
-			G.pb(to.y);
-			for (auto ed : G) eused[ed] = true;
-			tot.pb(G);
-			continue;
-		}
-		dfs(to.x, to.y, path);
-	}
-	path.pop_back();
-
-}
-
-short int go[100007][1007];
-
-bool cmp(int x, int y) {
-	return (w[x] < w[y]);
-}
-
-vector<int> color;
-int Q = 0;
-
-void dfs2(int vertex) {
-	color[vertex] = Q;
-	for (auto to : gr[vertex]) {
-		if (eused[to.y] || color[to.x] != -1) continue;
-		dfs2(to.x);
-	}
-}
-
-void solve(int test) {
-
-	cout << "Case " << test << ": ";
-
-	int n, m, k;
-	cin >> n >> m >> k;
-
-	gr.assign(n, {});
-	w.assign(m, -1);
-
-	for (int i = 0; i < m; ++i) {
-		int u, v;
-		cin >> u >> v >> w[i];
-		u--, v--;
-		gr[u].pb(mp(v, i));
-		gr[v].pb(mp(u, i));
-	}
-
-	used.assign(n, false);
-	eused.assign(m, false);
-	tot.clear();
-
-	int conn = 0;
-
-
-	for (int i = 0; i < n; ++i) {
-		if (!used[i]) {
-			conn++;
-			vector<pair<int, int> > path;
-			dfs(i, -1, path);
-		}
-	}
-
-	for (int i = 0; i < m; ++i) {
-		if (!eused[i]) {
-			vector<int> W;
-			W.pb(i);
-			tot.pb(W);
-		}
-	}
-
-	for (auto &x : tot) sort(all(x), cmp);
-	vector<ll> dp(k + 1, 1e18);
-	dp[0] = 0;
-	for (int i = 0; i < tot.size(); ++i) {
-		vector<ll> dp2(k + 1, 1e18);
-		for (int j = 0; j <= k; ++j) {
-			ll spend = 0;
-			short int best = -1;
-
-			for (int take = 0; take <= min(k + 1, (int) tot[i].size()); ++take) {
-				int R = take;
-				if (tot[i].size() > 1) R--;
-				if (R < 0) R++;
-
-				if (R <= j) {
-					ll W = dp[j - R] + spend;
-					if (W < dp2[j]) {
-						dp2[j] = W;
-						best = take;
-					}
-				}
-				else break;
-				if (take != tot[i].size()) spend += w[tot[i][take]];
-			}
-
-			go[i + 1][j] = best;
-		}
-		dp = dp2;
-	}
-
-	//cout << go[2][2] << endl;
-
-	vector<int> dlt;
-	int cur = max(k - conn, 0);
-
-	for (int i = tot.size(); i > 0; --i) {
-		int take = go[i][cur];
-		for (int j = 0; j < take; ++j) dlt.pb(tot[i - 1][j]);
-
-		if (take > 0 && tot[i - 1].size() > 1) take--;
-		cur -= take;
-	}
-
-	eused.assign(m, false);
-	for (auto x : dlt) {
-		eused[x] = true;
-	}
-	ll ans = 0;
-
-	for (int i = 0; i < m; ++i) if (eused[i]) {
-		ans += w[i];
-	}
-
-	color.assign(n, -1);
-
-	cout << ans << '\n';
-	Q=0;
-	for (int i = 0; i < n; ++i) {
-		if (color[i] == -1) {
-			dfs2(i);
-			if (Q + 1 < k) Q++;
-		}
-	}
-
-	// for (auto c : color) cout << c+1 << " ";
-	// cout << '\n';
-
-	
-
-} 
-
-int main(){
+int main()
 {
 	freopen("Data.in", "r", stdin);
     freopen("AcceptedRes.out", "w", stdout);
-#ifdef LOCAL
-	freopen("E_input.txt", "r", stdin);
-	//freopen("E_output.txt", "w", stdout);
-#endif
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+	scanf("%d%d", &n, &m);
+	facinv[0] = facinv[1] = fac[0] = mi[0] = 1;
+	for (int i = 2; i <= 100; i++)
+		facinv[i] = MOD - 1ll * MOD / i * facinv[MOD % i] % MOD;
+	for (int i = 1; i <= 100; i++)
+		mi[i] = 2ll * mi[i - 1] % MOD, fac[i] = 1ll * fac[i - 1] * i % MOD, facinv[i] = 1ll * facinv[i - 1] * facinv[i] % MOD;
+	dp[1][1][1] = 1;
+	for (int i = 2; i <= m + 1; i++)
+		for (int j = i; j <= n; j++)
+			for (int k = 1; k <= j - i + 1; k++)
+				for (int p = 1; p <= j - k - i + 2; p++)
+					dp[i][j][k] = (dp[i][j][k] + 1ll * dp[i - 1][j - k][p] * qmi(mi[p] - 1, k) % MOD * C(n - j + k, k) % MOD * qmi(2, C(k, 2)) % MOD) % MOD;
 
-	int t;
-	cin >> t;
-
-	for (int i = 0; i < t; ++i) solve(i + 1);
-
+	// for(int i = 1; i <= m; i++) {
+	// 	cout << endl;
+	// 	for(int j = 1; j <= n; j++) {
+	// 		for(int k = 1; k <= n; k++) {
+	// 			cout << i << "\t" << j << '\t' << k << ":\t" << dp[i][j][k] << endl;
+	// 		}
+	// 		cout << endl;
+	// 	}
+	// }
+	
+	// for (int i = 1; i <= n; i++)
+	// 	for (int j = 1; j <= n; j++)
+	// 		if (dp[k + 1][i][j])
+	for(int i = m; i <= n; i++)
+		for(int j = 1; j <= i - m + 1; j++)
+				ans = (ans + 1ll * dp[m + 1][i][j] * j % MOD * qmi(2, C(n - i, 2)) % MOD * qmi(2, 1ll * j * (n - i) % MOD)) % MOD;
+	printf("%lld\n", 1ll * ans * qmi(n - 1, MOD - 2) % MOD);
+	return 0;
 }
